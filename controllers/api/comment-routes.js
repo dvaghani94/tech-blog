d617073db21bd381f91a496d743a1ac6b0b8eb93
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-20 02:49:37 -0400

    working on comment-route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -31,4 +31,42 @@ router.get('/', async (req, res) => {
   }
 });
 
+// router.put('/:id', withAuth, async (req, res) => {
+//   try {
+//     const [affectedRows] = await Post.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//         user_id: req.sessions.user_id
+//       },
+//     });
+
+//     if (affectedRows > 0) {
+//       res.status(200).end();
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.delete('/:id', withAuth, async (req, res) => {
+//   try {
+//     const [affectedRows] = Post.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.sessions.user_id
+//       },
+//     });
+
+//     if (affectedRows > 0) {
+//       res.status(200).end();
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 module.exports = router;
