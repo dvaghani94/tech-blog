4f4a0910836d8860e31f56c40980b92f5074c4b2
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-20 01:31:46 -0400

    saving routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+// URL: /api/comment/
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      // TODO: COMMENT BODY IN REQUEST USING SPREAD
+      ...req.body,
+      // TODO: SET USERID TO SESSION LOGGEDIN USERID
+      user_id: req.session.user_id
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
